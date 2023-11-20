9b0be9276421405bc7b923ec427130c01789f645
Joshua Dogma <joshuadogma@Joshuas-MacBook-Pro.local>
2023-11-19 20:11:56 -0500

    Initial commit. Been working on this. Finally got the form to work, console logs US & PW with token

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import SignUpForm from './Components /SignUpForm';
+import Authenticate from './Components /Authenticate';
+
+function App() {
+
+  return (
+    <>
+    <SignUpForm />
+    <Authenticate />
+    </>
+  )
+}
+
+export default App
